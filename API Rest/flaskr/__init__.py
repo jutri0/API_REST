787298b8c5ef514b94b23b2e850daba8711aaf10
Hol1sPruev4Sen4 <juliand_triana@soy.sena.edu.co>
2023-08-27 05:11:20 -0500

    feat: Conexion con MySQL
    Se realizan ajustes para que funcione con MySQL en lugar de SQLite

--- API Rest/flaskr/__init__.py
+++ API Rest/flaskr/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+
+
 def create_app(config_name):
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/tutorial_canciones'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     return app
